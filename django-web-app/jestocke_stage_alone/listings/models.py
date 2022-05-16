089b553b12c39845c46b494081470d4999cd899a
sangmin <sangmin@jestocke.com>
2022-05-16 17:22:04 +0200

    makemigrations and migrate for models Class Band

--- django-web-app/jestocke_stage_alone/listings/models.py
+++ django-web-app/jestocke_stage_alone/listings/models.py
@@ -1,8 +1,28 @@
 from operator import mod
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+#
+# 1. Band
+#
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
 
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=100)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+
+#
+# 2. Listing
+#
 class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
